3dda1ee7861a6b5b71eaf577149d10c3912bb414
DavidQuirozG <daqui.99@hotmail.com>
2026-07-01 11:36:32 -0500

    Se finaliza calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,30 @@
-const prompt = require('prompt-sync');
+const prompt = require('prompt-sync')();
 
-let nombre = prompt("Cuál es tu nombre?")
-console.log(nombre)
+let numero1 = prompt("Ingresa el primer número: ")
+numero1 = Number(numero1)
+
+let operacion = prompt("Cuál operación deseas ejecutar (+,-,*,/): ");
+
+let numero2 = prompt("Ingresa el segundo número: ")
+numero2 = Number(numero2)
+
+let resultado = 0;
+
+if (operacion === "+") {
+    resultado = numero1 + numero2;
+    console.log(resultado)
+}
+else if (operacion === "-") {
+    resultado = numero1 - numero2;
+    console.log(resultado)
+}
+else if (operacion === "*") {
+    resultado = numero1 * numero2;
+    console.log(resultado)
+}
+else if (operacion === "/") {
+    resultado = numero1 / numero2;
+    console.log(resultado)
+}
+
+
