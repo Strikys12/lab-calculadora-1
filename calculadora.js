be393c1d958d7fa900fdfa9b0c94563a9369c270
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2026-07-01 12:48:48 -0500

    refactor

--- calculadora.js
+++ calculadora.js
@@ -1,47 +1,48 @@
 const prompt = require('prompt-sync')();
 let activo = true;
 
+const opcionesValidas = ["+", "-", "*", "/", "si", "no"]
 
 while (activo === true) {
-    let numero1 = prompt("Ingresa el primer número: ")
-    numero1 = Number(numero1)
-    let operacion = prompt("Cuál operación deseas ejecutar (+,-,*,/): ");
-    let numero2 = prompt("Ingresa el segundo número: ")
-    numero2 = Number(numero2)
-    let resultado = 0;
+  console.log()
+  console.log(" -------- Calculadora ---------");
+  console.log()
+  let operacion = prompt("Cuál operación deseas ejecutar (+,-,*,/): ")
 
+  let numero1 = Number(prompt("Ingresa el primer número: "))
+  let numero2 = Number(prompt("Ingresa el segundo número: "))
 
-    if (operacion === "+") {
-        resultado = numero1 + numero2;
-        console.log(resultado)
-    }
-    else if (operacion === "-") {
-        resultado = numero1 - numero2;
-        console.log(resultado)
-    }
-    else if (operacion === "*") {
-        resultado = numero1 * numero2;
-        console.log(resultado)
-    }
-    else if (operacion === "/") {
-        if (numero1 === 0 || numero2 === 0) {
-            console.log("No puedes dividir entre cero")
-        } else {
-            resultado = numero1 / numero2
-        }
+  if (!opcionesValidas.includes(operacion)) {
+    console.log("Opción no válida, intenta de nuevo")
+    continue
+  }
+
+  if (operacion === "+") {
+    console.log(`El resultado es ${numero1 + numero2}`)
+  }
+
+  if (operacion === "-") {
+    console.log(`El resultado es ${numero1 - numero2}`)
+  }
+
+  if (operacion === "*") {
+    console.log(`El resultado es ${numero1 * numero2}`)
+  }
+
+  if (operacion === "/") {
+    if (numero1 === 0 || numero2 === 0) {
+      console.log("No puedes dividir entre cero")
+    } else {
+      console.log(`El resultado es ${numero1 / numero2}`)
     }
     else { console.log("operación inválida") }
-
-
-    let respuesta = prompt("Desea hacer otra operación:")
+    let respuesta = prompt("Desea hacer otra operación")
     if (respuesta === "si") {
-        activo = true
+      activo = true
     }
 
-    else if (respuesta === "no") {
-        activo = false
+    if (respuesta.toLowerCase() === "no") {
+      activo = false
     }
-
-    else { console.log("respuesta invalida") }
-}
+  }
 
