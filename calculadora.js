7acf71c06b1873ff913e938cf0bd4a815f9fdd60
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2026-07-01 13:27:24 -0500

    refactor: saltos de linea y validaciones

--- calculadora.js
+++ calculadora.js
@@ -1,23 +1,25 @@
 const prompt = require('prompt-sync')();
-let activo = true;
 
+let activo = true;
 const opcionesValidas = ["+", "-", "*", "/", "si", "no"]
 
 while (activo === true) {
   console.log()
+  console.log('-------------------- Calculadora ---------------------')
+  console.log()
   let operacion = prompt("Cuál operación deseas ejecutar (+,-,*,/): ")
   console.log()
+
+  if (!opcionesValidas.includes(operacion)) {
+    console.log("\nOperación no válida, intenta de nuevo")
+    continue
+  }
 
   let numero1 = Number(prompt("Ingresa el primer número: "))
   let numero2 = Number(prompt("Ingresa el segundo número: "))
 
   if (Number.isNaN(numero1) || Number.isNaN(numero2)) {
-    console.log("Debes ingresar números válidos")
-    continue
-  }
-
-  if (!opcionesValidas.includes(operacion)) {
-    console.log("Opción no válida, intenta de nuevo")
+    console.log("\nDebes ingresar números válidos")
     continue
   }
 
@@ -37,7 +39,7 @@ while (activo === true) {
     const div = numero1 / numero2
 
     if (!Number.isFinite(div)) {
-      console.log("No puedes dividir entre cero")
+      console.log("\nNo puedes dividir entre cero")
       continue
     }
 
@@ -46,6 +48,11 @@ while (activo === true) {
 
   console.log()
   let respuesta = prompt("Desea hacer otra operación (si/no): ")
+
+  while (!["si", "no"].includes(respuesta.toLowerCase())) {
+    console.log("\nOpción no válida, intenta de nuevo")
+    respuesta = prompt("Desea hacer otra operación (si/no): ")
+  }
 
   if (respuesta.toLowerCase() === "si") {
     activo = true
